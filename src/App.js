873619fb34f7f934ba27ca5231fc579ad1b425f2
Shresth Raj Gupta <shresthgupta.phy21@kgpian.iitkgp.ac.in>
2023-03-08 01:32:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [quotes, setQuotes] = useState([]);
+  const [quote, setQuote] = useState("")
+
+  function getRandomQuote() {
+    return setQuote(quotes[Math.floor(Math.random() * quotes.length)])
+  }
+
+  useEffect(() => {
+    fetch("https://type.fit/api/quotes")
+      .then((response) => response.json())
+      .then((jsonData) => {
+        setQuotes(jsonData)
+        setQuote(jsonData[Math.floor(Math.random() * jsonData.length)])
+      })
+  }, [])
+
+  console.log(quotes);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="heading">
+        <h1>Quote Generator</h1>
+      </div>
+
+      <div className="box">
+
+        <div className="content">
+
+          <div className="btn"><button onClick={getRandomQuote}>
+            New Quote
+          </button></div>
+
+
+          <div className="quote">
+            <div className="text"><h2>{quote.text}</h2></div>
+            <div className="author"><h3>{quote.author}</h3></div>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
